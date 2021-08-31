d8e07affdd0627e1af2107419b5fda23a8920136
michalgwozdz <michalgwozdz@payability.com>
2021-08-31 18:12:45 +0200

    michalgwozdz/no-jira/integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,26 @@ import {Component} from '@angular/core';
 export class AppComponent {
   readonly url: string = 'http://localhost:3000/main.js';
 
-  constructor() {
-    // @ts-ignore
-    console.log(PybIntegrationUtils)
+  loading: boolean = false;
+  // @ts-ignore
+  integration: PybMpIntegration = null;
+
+  configuration = {
+    logo: 'test',
+    label: 'test',
+    mpKey: 'test',
+    login: 'test'
+  }
+
+  setIntegration(): void {
+    this.integration = {
+      authId: 'test',
+      password: 'test',
+      login: 'test'
+    }
+  }
+
+  setLoading($event: boolean): void {
+    this.loading = $event;
   }
 }
